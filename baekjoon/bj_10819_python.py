82b2c19a634141f6a6464f4cf96ab1eaab070030
devejs <ssej0221@gmail.com>
2021-04-06 23:39:26 +0900

    Update 210406 Baekjoon Python

---
+++ baekjoon/bj_10819_python.py
@@ -0,0 +1,21 @@
+def dfs(d, v, sum):
+    global num, result
+    if d == num-1:
+        if result < sum:
+            result = sum
+        return
+    for i in range(num):
+        if not checked[i]:
+            checked[i] = 1
+            dfs(d+1, arr[i], sum+abs(arr[i]-v))
+            checked[i] = 0
+
+num = int(input())
+arr = list(map(int, input().split()))
+checked = [0 for _ in range(num)]
+result = 0
+for i in range(num):
+    checked[i] = 1
+    dfs(0, arr[i], 0)
+    checked[i] = 0
+print(result)
